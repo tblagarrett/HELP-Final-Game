19bfeedc3ed2a287e656763c5b4a3b7cacc6c4ca
ljcui1 <yangyangjiaocui@gmail.com>
2024-02-22 17:26:24 -0800

    HE MOVES

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed;
+    float speedX, speedY;
+    private Rigidbody2D rb;
+
+    private static PlayerManager _instance; // make a static private variable of the component data type
+    public static PlayerManager Instance { get { return _instance; } } // make a public way to access the private variable\
+    public GameObject Player { get { return GameObject.FindWithTag("Player"); } }
 
     private void Awake()
     {
@@ -21,12 +28,14 @@ public class PlayerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
     }
 }
