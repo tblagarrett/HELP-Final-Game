198ca3297e424583ad3c89aabf634a423eb94db5
kvanarsd <kvanarsd@ucsc.edu>
2024-02-22 19:06:14 -0800

    Hunger decay

--- Assets/Scripts/MonsterManager.cs
+++ Assets/Scripts/MonsterManager.cs
@@ -7,15 +7,22 @@ public class MonsterManager : MonoBehaviour
 {
     // prefab of Monster
     [SerializeField] private MonsterScript MonsterPrefab;
-    public MonsterScript Monster;
 
+    public MonsterScript Monster;
     public GameObject Parent;
+
+    // variables for hunger and healh decay
+    [SerializeField] private float hungerDelay;
+    [SerializeField] private float healthDelay;
+    [SerializeField] private int subHunger;
+    [SerializeField] private int subHealth;
 
     //public PlayerManager Player;
 
     void Start()
     {
         Monster = Instantiate(MonsterPrefab, Parent.transform);
+        HungerDecay();
     }
 
     // Update is called once per frame
@@ -24,6 +31,23 @@ public class MonsterManager : MonoBehaviour
 
     }
 
+    public IEnumerator HungerDecay()
+    {
+        yield return new WaitForSeconds(hungerDelay);
+        Monster.hunger -= subHunger;
+        
+        if (Monster.hunger <= 0)
+        {
+            HealthDecay();
+        }
+
+        HungerDecay();
+    }
+    public IEnumerator HealthDecay()
+    {
+        Monster.health -= subHealth;
+        yield return new WaitForSeconds(healthDelay);
+    }
     public void SetMax()
     {
         Monster.curVisRadius = Monster.maxVisRadius;
