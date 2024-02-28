9f63977b652b7b5ad38a79652091c00343182448
ljcui1 <yangyangjiaocui@gmail.com>
2024-02-27 17:14:46 -0800

    bro spins!

--- Assets/Scripts/PlayerScripts.cs
+++ Assets/Scripts/PlayerScripts.cs
@@ -7,10 +7,11 @@ public class PlayerScripts : MonoBehaviour
 
     public int health;
     public int hunger;
+    public SpriteRenderer sRen;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sRen = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
