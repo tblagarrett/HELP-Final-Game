fc99b9a711d09082a4e82ba5e554ce66a1f8c011
tblagarrett <tblagarrett@gmail.com>
2024-03-07 16:03:17 -0800

    Coroutine works, need to add to player manager

--- Assets/Scripts/VignetteClass.cs
+++ Assets/Scripts/VignetteClass.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
 public class VignetteClass : MonoBehaviour
 {
+    private Volume volumeComponent;
     private Vignette vignetteComponent;
-    private string direction = "increase";
+    private string directionIntensity = "increase";
     public float intensityChange;
     public bool isPlaying = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        vignetteComponent = GetComponent<Vignette>();
+        volumeComponent = GetComponent<Volume>();
+        volumeComponent.profile.TryGet(out vignetteComponent);
+        
         vignetteComponent.color.Override(Color.black);
         vignetteComponent.intensity.Override(.288f);
     }
@@ -30,13 +35,38 @@ public class VignetteClass : MonoBehaviour
         vignetteComponent.color.Override(Color.red);
         for (;;)
         {
+            float currentIntensity = vignetteComponent.intensity.GetValue<float>();
+
             // If it is too instense, swap to decreasing
-            if (direction == "increase" && vignetteComponent.intensity.GetValue<float>() > .5f)
+            if (directionIntensity == "increase" && currentIntensity > .5f)
             {
-                direction = "decrease";
+                directionIntensity = "decrease";
+            }
+            // If it reaches the bottom threshold, swap to increasing
+            if (directionIntensity == "decrease" && currentIntensity < .288f)
+            {
+                directionIntensity = "increase";
+            }
+
+            // Adjust the intensity as necessary
+            if (directionIntensity == "increase")
+            {
+                vignetteComponent.intensity.Override(currentIntensity + intensityChange);
+            }
+            if (directionIntensity == "decrease")
+            {
+                vignetteComponent.intensity.Override(currentIntensity - intensityChange);
             }
 
             yield return new WaitForSeconds(.1f);
         }
+    }
+
+    public void StopVignetteHurt()
+    {
+        isPlaying = false;
+        StopCoroutine(PlayVignetteHurt());
+        vignetteComponent.color.Override(Color.black);
+        vignetteComponent.intensity.Override(.288f);
     }
 }
